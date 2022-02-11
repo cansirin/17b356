685c8c9efe0353f958b04e6df9fe7a59693db417
Can Sirin <cansirin12@gmail.com>
2022-02-11 11:36:57 -0800

    update mutable operations to immutable

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -14,7 +14,7 @@ export const addMessageToStore = (state, payload) => {
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
       const newConvo = { ...convo };
-      newConvo.messages.push(message);
+      newConvo.messages = [...convo.messages, message];
       newConvo.latestMessageText = message.text;
       return newConvo;
     } else {
@@ -70,10 +70,10 @@ export const addSearchedUsersToStore = (state, users) => {
 export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
     if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return { ...convo };
+      const newConvo = { ...convo };
+      newConvo.messages = [...convo.messages, message];
+      newConvo.latestMessageText = message.text;
+      return newConvo;
     } else {
       return convo;
     }
